8db6790d39dc6dd973f8a7876e2eb3764478100c
Mgbedoro Chinedu <69406478+mgbedorochinedu@users.noreply.github.com>
2022-05-12 23:29:13 +0100

    Add project files.

---
+++ CSHARPLINQ/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//......................................................................................................
+
+Console.WriteLine("Using Existing .Net Features!");
+
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+
+foreach (int number in numbers)
+{
+    if(number < 3)
+    Console.WriteLine(number);
+}
+
+
+//....................................................................................................
+
+Console.WriteLine("Using LINQ");
+
+//Step 1: Getting data source
+List<int> numbersLinq = new List<int> { 1, 2, 3, 4, 5, 6 };
+
+//Step 2: Writing query
+var query = from n in numbersLinq
+            where n < 3
+            select n;
+//Step 3: Excuting the query
+foreach (int number in query)
+{
+    Console.WriteLine(number);
+}
+
+//........................................................................................................
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
