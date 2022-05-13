f1bc8a9c1990349aacd428db9fc0c01f5d634022
Mgbedoro Chinedu <69406478+mgbedorochinedu@users.noreply.github.com>
2022-05-13 01:16:56 +0100

    Steps of a Linq operation

--- CSHARPLINQ/Program.cs
+++ CSHARPLINQ/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+//SECTION ONE:
 //......................................................................................................
 
 Console.WriteLine("Using Existing .Net Features!");
@@ -21,9 +22,9 @@ Console.WriteLine("Using LINQ");
 List<int> numbersLinq = new List<int> { 1, 2, 3, 4, 5, 6 };
 
 //Step 2: Writing query
-var query = from n in numbersLinq
-            where n < 3
-            select n;
+IEnumerable<int> query = from n in numbersLinq   //Part 1: Data Source
+            where n < 3                          //Part 2: Filter
+            select n;                            //Part 3: Select
 //Step 3: Excuting the query
 foreach (int number in query)
 {
@@ -33,8 +34,22 @@ foreach (int number in query)
 //........................................................................................................
 
 
+//Problem Statements:
 
+// Write a simple C# program to declare a list of integer values, followed by a simple LINQ query to check for even numbers, and finally print them to the console.
+Console.WriteLine("Even Number Solutions");
+List<int> inputs = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+IEnumerable<int> queryNumber = from n in inputs
+                               where n % 2 == 0
+                               select n;
+
+foreach(int evenNumber in queryNumber)
+{
+    Console.WriteLine(evenNumber);
+}
+
+//..................................................................................................
 
 
 
